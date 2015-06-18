268fbe4905ee69d089ac88699698d8c1b8836e5e
Avner Barr <avner@insert.io>
2015-06-18 14:01:38 +0300

    initial commit

---
+++ TestLibrary/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  TestLibrary
+//
+//  Created by Avner Barr on 6/18/15.
+//  Copyright (c) 2015 Avner Barr. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
